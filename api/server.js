2fd8deeeb463587e8953e59c25a95cd3e583a9e7
Adam Wangrat <amvangradt@gmail.com>
2018-08-07 13:49:49 +0200

    add next and previous months to json-server #11

--- api/server.js
+++ api/server.js
@@ -1,21 +1,11 @@
 var jsonServer = require('json-server')
+var moment = require('moment')
+var _ = require('lodash')
 var server = jsonServer.create();
 var router = jsonServer.router('api/db.json');
 var middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-
-function year() {
-    return new Date().getFullYear();
-}
-
-function month() {
-    return ("0" + (new Date().getMonth() + 1)).slice(-2);
-}
-
-function day() {
-    return ("0" + new Date().getDate()).slice(-2);
-}
 
 server.get('/endpoints/v1/authentication/status', function (req, res) {
     res.jsonp({
@@ -27,7 +17,9 @@ server.get('/endpoints/v1/authentication/status', function (req, res) {
     })
 });
 
-server.get('/endpoints/v1/calendar/' + year() + '/' + month() + '/work-log/entries', function (req, res) {
+
+
+server.get('/endpoints/v1/calendar/' + moment().format('YYYY') + '/' + moment().format('MM') + '/work-log/entries', function (req, res) {
     res.jsonp({
         items: [
             {
@@ -35,7 +27,7 @@ server.get('/endpoints/v1/calendar/' + year() + '/' + month() + '/work-log/entri
                 id: "1",
                 workload: "1d",
                 employee: "john.doe",
-                day: year() + "/" + month() + "/" + day(),
+                day: moment().format('YYYY') + "/" + moment().format('MM') + "/" + moment().format('DD'),
                 projectNames: ["vacation"]
             },
             {
@@ -43,7 +35,7 @@ server.get('/endpoints/v1/calendar/' + year() + '/' + month() + '/work-log/entri
                 id: "3",
                 workload: "2h",
                 employee: "john.doe",
-                day: year() + "/" + month() + "/" + day(),
+                day: moment().format('YYYY') + "/" + moment().format('MM') + "/" + moment().format('DD'),
                 projectNames: ["internal", "conference"]
             },
             {
@@ -51,7 +43,7 @@ server.get('/endpoints/v1/calendar/' + year() + '/' + month() + '/work-log/entri
                 id: "4",
                 workload: "2h",
                 employee: "bart.simson",
-                day: year() + "/" + month() + "/" + day(),
+                day: moment().format('YYYY') + "/" + moment().format('MM') + "/" + moment().format('DD'),
                 projectNames: ["internal", "conference"]
             },
             {
@@ -59,7 +51,7 @@ server.get('/endpoints/v1/calendar/' + year() + '/' + month() + '/work-log/entri
                 id: "5",
                 workload: "4h",
                 employee: "bart.simson",
-                day: year() + "/" + month() + "/" + day(),
+                day: moment().format('YYYY') + "/" + moment().format('MM') + "/" + moment().format('DD'),
                 projectNames: ["internal", "brown-bag"]
             }
         ]
@@ -70,45 +62,58 @@ server.get('/endpoints/v1/projects', function (req, res) {
     res.jsonp(["OpenTrapp","Other","brown-bag","coaching","coding-dojo","conference","vacation","internal"])
 });
 
-server.get('/endpoints/v1/calendar/' + year() + '/' + month(), function (req, res) {
-    res.jsonp(
-        {
-            "id": ""+year()+"/"+month(),
-            "link": "/endpoints/v1/calendar/"+year()+"/"+month()+"",
-            "next": {"link": "/endpoints/v1/calendar/"+year()+"/"+month()+1}, "prev": {"link": "/endpoints/v1/calendar/"+year()+"/"+month(-1)},
-            "days": [
-                {"link": "/endpoints/v1/calendar/"+year()+"/"+month()+"/01", "id": ""+year()+"/"+month()+"/01", "holiday": false},
-                {"link": "/endpoints/v1/calendar/"+year()+"/"+month()+"/02", "id": ""+year()+"/"+month()+"/02", "holiday": false},
-                {"link": "/endpoints/v1/calendar/"+year()+"/"+month()+"/03", "id": ""+year()+"/"+month()+"/03", "holiday": false},
-                {"link": "/endpoints/v1/calendar/"+year()+"/"+month()+"/04", "id": ""+year()+"/"+month()+"/04", "holiday": true},
-                {"link": "/endpoints/v1/calendar/"+year()+"/"+month()+"/05", "id": ""+year()+"/"+month()+"/05", "holiday": true},
-                {"link": "/endpoints/v1/calendar/"+year()+"/"+month()+"/06", "id": ""+year()+"/"+month()+"/06", "holiday": false},
-                {"link": "/endpoints/v1/calendar/"+year()+"/"+month()+"/07", "id": ""+year()+"/"+month()+"/07", "holiday": false},
-                {"link": "/endpoints/v1/calendar/"+year()+"/"+month()+"/08", "id": ""+year()+"/"+month()+"/08", "holiday": false},
-                {"link": "/endpoints/v1/calendar/"+year()+"/"+month()+"/09", "id": ""+year()+"/"+month()+"/09", "holiday": false},
-                {"link": "/endpoints/v1/calendar/"+year()+"/"+month()+"/10", "id": ""+year()+"/"+month()+"/10", "holiday": false},
-                {"link": "/endpoints/v1/calendar/"+year()+"/"+month()+"/11", "id": ""+year()+"/"+month()+"/11", "holiday": true},
-                {"link": "/endpoints/v1/calendar/"+year()+"/"+month()+"/12", "id": ""+year()+"/"+month()+"/12", "holiday": true},
-                {"link": "/endpoints/v1/calendar/"+year()+"/"+month()+"/13", "id": ""+year()+"/"+month()+"/13", "holiday": false},
-                {"link": "/endpoints/v1/calendar/"+year()+"/"+month()+"/14", "id": ""+year()+"/"+month()+"/14", "holiday": false},
-                {"link": "/endpoints/v1/calendar/"+year()+"/"+month()+"/15", "id": ""+year()+"/"+month()+"/15", "holiday": false},
-                {"link": "/endpoints/v1/calendar/"+year()+"/"+month()+"/16", "id": ""+year()+"/"+month()+"/16", "holiday": false},
-                {"link": "/endpoints/v1/calendar/"+year()+"/"+month()+"/17", "id": ""+year()+"/"+month()+"/17", "holiday": false},
-                {"link": "/endpoints/v1/calendar/"+year()+"/"+month()+"/18", "id": ""+year()+"/"+month()+"/18", "holiday": true},
-                {"link": "/endpoints/v1/calendar/"+year()+"/"+month()+"/19", "id": ""+year()+"/"+month()+"/19", "holiday": true},
-                {"link": "/endpoints/v1/calendar/"+year()+"/"+month()+"/20", "id": ""+year()+"/"+month()+"/20", "holiday": false},
-                {"link": "/endpoints/v1/calendar/"+year()+"/"+month()+"/21", "id": ""+year()+"/"+month()+"/21", "holiday": false},
-                {"link": "/endpoints/v1/calendar/"+year()+"/"+month()+"/22", "id": ""+year()+"/"+month()+"/22", "holiday": false},
-                {"link": "/endpoints/v1/calendar/"+year()+"/"+month()+"/23", "id": ""+year()+"/"+month()+"/23", "holiday": false},
-                {"link": "/endpoints/v1/calendar/"+year()+"/"+month()+"/24", "id": ""+year()+"/"+month()+"/24", "holiday": false},
-                {"link": "/endpoints/v1/calendar/"+year()+"/"+month()+"/25", "id": ""+year()+"/"+month()+"/25", "holiday": true},
-                {"link": "/endpoints/v1/calendar/"+year()+"/"+month()+"/26", "id": ""+year()+"/"+month()+"/26", "holiday": true},
-                {"link": "/endpoints/v1/calendar/"+year()+"/"+month()+"/27", "id": ""+year()+"/"+month()+"/27", "holiday": false},
-                {"link": "/endpoints/v1/calendar/"+year()+"/"+month()+"/28", "id": ""+year()+"/"+month()+"/28", "holiday": false},
-                {"link": "/endpoints/v1/calendar/"+year()+"/"+month()+"/29", "id": ""+year()+"/"+month()+"/29", "holiday": false},
-                {"link": "/endpoints/v1/calendar/"+year()+"/"+month()+"/30", "id": ""+year()+"/"+month()+"/30", "holiday": false},
-                {"link": "/endpoints/v1/calendar/"+year()+"/"+month()+"/31", "id": ""+year()+"/"+month()+"/31", "holiday": false}]
-        })
+server.get('/endpoints/v1/calendar/' + moment().format('YYYY') + '/' + moment().format('MM'), function (req, res) {
+    month = {
+        "id": ""+moment().format('YYYY')+"/"+moment().format('MM'),
+        "link": "/endpoints/v1/calendar/"+moment().format('YYYY')+"/"+moment().format('MM')+"",
+        "next": {"link": "/endpoints/v1/calendar/"+moment().add(1,'month').format('YY')+"/"+moment().add(1,'month').format('MM')}, "prev": {"link": "/endpoints/v1/calendar/"+moment().subtract(1,'month').format('YY')+"/"+moment().subtract(1,'month').format('MM')},
+        "days": []
+    }
+
+    var monthDate = moment().startOf('month');
+
+    _.times(monthDate.daysInMonth('MM'), function(n) {
+        month.days.push({"link": "/endpoints/v1/calendar/"+moment().format('YYYY')+"/"+moment().format('MM')+"/"+monthDate.format('DD'), "id": ""+moment().format('YYYY')+"/"+moment().format('MM')+"/"+monthDate.format('DD'), "holiday": monthDate.weekday() == 0 || monthDate.weekday() == 6})
+        monthDate.add(1, 'day');
+    })
+
+    res.jsonp(month);
+});
+
+server.get('/endpoints/v1/calendar/' + moment().add(1, 'month').format('YYYY') + '/' + moment().add(1, 'month').format('MM'), function (req, res) {
+    month = {
+        "id": ""+moment().add(1, 'month').format('YYYY')+"/"+moment().add(1, 'month').format('MM'),
+        "link": "/endpoints/v1/calendar/"+moment().add(1, 'month').format('YYYY')+"/"+moment().add(1, 'month').format('MM')+"",
+        "next": {"link": "/endpoints/v1/calendar/"+moment().add(2,'month').format('YY')+"/"+moment().add(2,'month').format('MM')}, "prev": {"link": "/endpoints/v1/calendar/"+moment().format('YY')+"/"+moment().format('MM')},
+        "days": []
+    }
+
+    var monthDate = moment().add(1, 'month').startOf('month');
+
+    _.times(monthDate.daysInMonth('MM'), function(n) {
+        month.days.push({"link": "/endpoints/v1/calendar/"+moment().add(1, 'month').format('YYYY')+"/"+moment().add(1, 'month').format('MM')+"/"+monthDate.format('DD'), "id": ""+moment().add(1, 'month').format('YYYY')+"/"+moment().add(1, 'month').format('MM')+"/"+monthDate.format('DD'), "holiday": monthDate.weekday() == 0 || monthDate.weekday() == 6})
+        monthDate.add(1, 'day');
+    })
+
+    res.jsonp(month);
+});
+
+server.get('/endpoints/v1/calendar/' + moment().subtract(1, 'month').format('YYYY') + '/' + moment().subtract(1, 'month').format('MM'), function (req, res) {
+    month = {
+        "id": ""+moment().subtract(1, 'month').format('YYYY')+"/"+moment().subtract(1, 'month').format('MM'),
+        "link": "/endpoints/v1/calendar/"+moment().subtract(1, 'month').format('YYYY')+"/"+moment().subtract(1, 'month').format('MM')+"",
+        "next": {"link": "/endpoints/v1/calendar/"+moment().format('YY')+"/"+moment().format('MM')}, "prev": {"link": "/endpoints/v1/calendar/"+moment().subtract(2,'month').format('YY')+"/"+moment().subtract(2,'month').format('MM')},
+        "days": []
+    }
+
+    var monthDate = moment().subtract(1, 'month').startOf('month');
+
+    _.times(monthDate.daysInMonth('MM'), function(n) {
+        month.days.push({"link": "/endpoints/v1/calendar/"+moment().subtract(1, 'month').format('YYYY')+"/"+moment().subtract(1, 'month').format('MM')+"/"+monthDate.format('DD'), "id": ""+moment().subtract(1, 'month').format('YYYY')+"/"+moment().subtract(1, 'month').format('MM')+"/"+monthDate.format('DD'), "holiday": monthDate.weekday() == 0 || monthDate.weekday() == 6})
+        monthDate.add(1, 'day');
+    })
+
+    res.jsonp(month);
 });
 
 
