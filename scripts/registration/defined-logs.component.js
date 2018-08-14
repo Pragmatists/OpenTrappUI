378e98fdcf5254b97db8932331f4ee35fa76b24e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-08-14 08:38:49 +0000

    Deploy Pragmatists/OpenTrappUI to github.com/Pragmatists/OpenTrappUI.git:gh-pages

---
+++ scripts/registration/defined-logs.component.js
@@ -0,0 +1,26 @@
+(function () {
+
+    angular
+        .module('openTrapp.registration')
+        .component('otDefinedLogs', {
+            templateUrl: 'templates/registration/defined-logs.component.html',
+            controller: DefinedLogsController,
+            controllerAs: 'viewModel',
+            bindings: {
+                setLog: '='
+            }
+        });
+
+    function DefinedLogsController(definedLogs) {
+        var self = this;
+        this.deleteLog = deleteLog;
+        
+        angular.extend(self, {
+            definedLogs: definedLogs
+        });
+
+        function deleteLog(i) {
+            definedLogs.deleteLog(i);
+        }
+    }
+})();
